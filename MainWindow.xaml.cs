2fc686ec448ebca0cfe1e0364faa450ebbf89f06
Martin Wattenberg <martin.wattenberg@infineon.com>
2025-08-21 22:02:54 +0200

    Separating logic out of MainWindow

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,11 +20,7 @@ namespace SerialPlotDN_WPF
     /// </summary> 　
     public partial class MainWindow : Window
     {
-        private readonly ScottPlot.Plottables.Signal[] _signals = new ScottPlot.Plottables.Signal[8]; // 8 channels   
-        // Pre-allocated arrays for linearized data - avoid memory allocations during plotting
-        private readonly double[][] _linearizedDataArrays = new double[8][];
-        // Timer for updating the plot at a fixed rate
-        readonly private System.Timers.Timer _updatePlotTimer = new() { Interval = 33, Enabled = true, AutoReset = true }; // ~30 FPS
+        private DataAndPlotManager _plotManager;
         private readonly System.Timers.Timer _updateAquisitionTimer = new() { Interval = 1000, Enabled = true, AutoReset = true }; // 500 ms
         
         private TimeSpan _prevCpuTime = TimeSpan.Zero;
@@ -36,7 +32,6 @@ namespace SerialPlotDN_WPF
 
         // Configurable display settings - use DataStream's ring buffer capacity
         public int DisplayElements { get; set; } = 3000; // Number of elements to display
-        private readonly int _channelCount = 8;
 
         //Debug just data parsing and plotting
         private DataStream dataStream;
@@ -48,22 +43,16 @@ namespace SerialPlotDN_WPF
         public MainWindow()
         {
             InitializeComponent();
-
-            // Initialize pre-allocated arrays for efficient data copying
-            for (int i = 0; i < _channelCount; i++)
-            {
-                _linearizedDataArrays[i] = new double[DisplayElements];
-            }
-            
-            InitializePlot();
+            _plotManager = new DataAndPlotManager(WpfPlot1, ChannelControlBar, VerticalControl, HorizontalControl);
+            _plotManager.InitializePlot();
+            _plotManager.InitializeChannelControlBar();
+            _plotManager.SetupPlotUserInput();
             InitializeDataStream();
             InitializeTimer();
-            InitializeChannelControlBar();
             InitializeHorizontalControl();
             InitializeVerticalControl();
             readSettingsXML();
             InitializeEventHandlers();
-            SetupPlotUserInput();
         }
 
         void InitializeHorizontalControl()
@@ -76,157 +65,28 @@ namespace SerialPlotDN_WPF
         {
             VerticalControl.Max = Ymax; // Set initial max value
             VerticalControl.Min = Ymin; // Set initial min value
-                                        // 
-
         }
 
         private void InitializeEventHandlers()
         {
-            HorizontalControl.WindowSizeChanged += HorizontalControl_WindowSizeChanged;
+            HorizontalControl.WindowSizeChanged += _plotManager.OnWindowSizeChanged;
+            VerticalControl.MinValueChanged += (s, v) => _plotManager.SetYLimits(v, _plotManager.Ymax);
+            VerticalControl.MaxValueChanged += (s, v) => _plotManager.SetYLimits(_plotManager.Ymin, v);
+            VerticalControl.AutoScaleChanged += (s, isAutoScale) => { if (!isAutoScale) _plotManager.SetYLimits(_plotManager.Ymin, _plotManager.Ymax); };
             RunControl.RunStateChanged += RunControl_RunStateChanged;
-            VerticalControl.MinValueChanged += VerticalControl_MinValueChanged;
-            VerticalControl.MaxValueChanged += VerticalControl_MaxValueChanged;
-            VerticalControl.AutoScaleChanged += VerticalControl_AutoScaleChanged;
-        }
-
-        private void HorizontalControl_WindowSizeChanged(object? sender, int newSize)
-        {
-            DisplayElements = newSize;
-            bool isRunning = _updatePlotTimer.Enabled;
-            if (_updatePlotTimer.Enabled)
-                _updatePlotTimer.Stop();
-            
-            for (int i = 0; i < _channelCount; i++)
-            {
-                var colorOld = _signals[i].Color;
-                var linewidthOld = _signals[i].LineWidth;
-                var antiAliasingOld = _signals[i].LineStyle.AntiAlias;
-                WpfPlot1.Plot.Remove(_signals[i]); // Remove old signals
-                _linearizedDataArrays[i] = new double[DisplayElements];
-                _signals[i] = WpfPlot1.Plot.Add.Signal(_linearizedDataArrays[i]);
-                _signals[i].Color = colorOld;
-                _signals[i].LineWidth = linewidthOld; // Reapply old line width
-                _signals[i].LineStyle.AntiAlias = antiAliasingOld; // Reapply old anti-aliasing setting
-            }
-            WpfPlot1.Plot.Axes.SetLimitsX(0, DisplayElements);
-            
-            if(isRunning ) 
-                _updatePlotTimer.Start();
-            else
-            {
-                UpdatePlot(null, null);
-            }
-                
         }
 
         private void RunControl_RunStateChanged(object? sender, RunControl.RunStates newState)
         {
             if (newState == RunControl.RunStates.Running)
             {
-                // Start the data stream and timers
                 dataStream.Start();
-                _updatePlotTimer.Start();
-                _updateAquisitionTimer.Start();
+                _plotManager.StartPlotTimer();
             }
             else
             {
-                // Stop the data stream and timers
                 dataStream.Stop();
-                _updatePlotTimer.Stop();
-                _updateAquisitionTimer.Stop();
-            }
-        }
-
-        private void VerticalControl_MinValueChanged(object? sender, int newMin)
-        {
-            Ymin = newMin;
-            WpfPlot1.Plot.Axes.SetLimitsY(Ymin, Ymax);
-            WpfPlot1.Refresh();
-        }
-
-        private void VerticalControl_MaxValueChanged(object? sender, int newMax)
-        {
-            Ymax = newMax;
-            WpfPlot1.Plot.Axes.SetLimitsY(Ymin, Ymax);
-            WpfPlot1.Refresh();
-        }
-
-        private void VerticalControl_AutoScaleChanged(object? sender, bool isAutoScale)
-        {
-            if (!isAutoScale)
-            {
-                WpfPlot1.Plot.Axes.SetLimitsY(Ymin, Ymax);
-            }
-            WpfPlot1.Refresh();
-        }
-
-        private void InitializePlot()
-        {
-            WpfPlot1.Plot.Clear();
-            WpfPlot1.Plot.Add.Palette = new ScottPlot.Palettes.Category10();
-
-            // Double the axis font size for all axes (ScottPlot v5+)
-            foreach (var axis in WpfPlot1.Plot.Axes.GetAxes())
-            {
-                axis.TickLabelStyle.FontSize *= 2;
-                axis.Label.FontSize *= 2;
-            }
-
-            // Initialize Signal plottables with pre-allocated arrays
-            for (int i = 0; i < _channelCount; i++)
-            {
-                // Create Signal plottables with pre-allocated arrays - these will be reused
-                _signals[i] = WpfPlot1.Plot.Add.Signal(_linearizedDataArrays[i]);
-                _signals[i].Color = _signals[i].Color.Lighten(0.2); // Lighten the color for better 
-                _signals[i].Color = _signals[i].Color.WithOpacity(1.0); // Ensure full opacity
-                _signals[i].LineWidth = 1; // Thinner lines = better performance
-                
-                // Disable anti-aliasing for better performance
-                _signals[i].LineStyle.AntiAlias = false;
-            }
-
-            WpfPlot1.Plot.ShowLegend();
-
-            // change figure colors
-            WpfPlot1.Plot.FigureBackground.Color = ScottPlot.Color.FromHex("#181818");
-            WpfPlot1.Plot.DataBackground.Color = ScottPlot.Color.FromHex("#1f1f1f");
-
-            // change axis and grid colors
-            WpfPlot1.Plot.Axes.Color(ScottPlot.Color.FromHex("#d7d7d7"));
-            WpfPlot1.Plot.Grid.LineWidth = 1.0f; // Thinner grid lines for better 
-            WpfPlot1.Plot.Grid.MajorLineColor = ScottPlot.Color.FromHex("#404040");
-
-            // change legend colors
-            WpfPlot1.Plot.Legend.BackgroundColor = ScottPlot.Color.FromHex("#404040");
-            WpfPlot1.Plot.Legend.FontColor = ScottPlot.Color.FromHex("#d7d7d7");
-            WpfPlot1.Plot.Legend.OutlineColor = ScottPlot.Color.FromHex("#d7d7d7");
-
-            // Optimize plot settings for performance
-            WpfPlot1.Plot.Axes.ContinuouslyAutoscale = false; // Manual scaling is faster
-            WpfPlot1.Plot.RenderManager.ClearCanvasBeforeEachRender = true;
-
-            // Set fixed axis limits to prevent auto scaling - show display window
-            WpfPlot1.Plot.Axes.SetLimitsX(0, DisplayElements);
-            WpfPlot1.Plot.Axes.SetLimitsY(Ymin, Ymax); // Adjust Y range based on your data
-            WpfPlot1.Plot.Axes.Bottom.IsVisible = true;
-        }
-
-        private void InitializeChannelControlBar()
-        {
-            // Create 8 channels with different colors
-            for (int i = 0; i < 8; i++)
-            {
-                ChannelControl channel = new ChannelControl();
-                var channelColor = _signals[i].Color;
-                var wpfColor = Color.FromArgb(channelColor.A,
-                                      channelColor.R,
-                                      channelColor.G,
-                                      channelColor.B);
-                channel.Color = wpfColor;
-                channel.Label = $"CH{i + 1}";
-                channel.Gain = 1.0;
-                channel.Offset = 0.0;
-                ChannelControlBar.AddChannel(channel); // Direct access to ChannelControlBar
+                _plotManager.StopPlotTimer();
             }
         }
 
@@ -234,35 +94,15 @@ namespace SerialPlotDN_WPF
         {
             SourceSetting sourceSetting = new SourceSetting("COM22", 1000000, 9);
             byte[] startbytes = new byte[] { 0xAA, 0xAA };
-            dataStream = new DataStream(sourceSetting, new DataParser(DataParser.BinaryFormat.uint16_t, _channelCount, startbytes));
+            dataStream = new DataStream(sourceSetting, new DataParser(DataParser.BinaryFormat.uint16_t, 8, startbytes));
             AquisitionControl.Baudrate = sourceSetting.BaudRate;
             
         }
 
         private void InitializeTimer()
         {
-            _updatePlotTimer.Elapsed += UpdatePlot;
             _updateAquisitionTimer.Elapsed += UpdateAquisition;
         }
-
-        private void UpdatePlot(object? source, ElapsedEventArgs? e)
-        {
-            // Use Dispatcher.BeginInvoke for better performance
-            Application.Current.Dispatcher.BeginInvoke(() =>
-            {
-                for (int channel = 0; channel < _channelCount; channel++)
-                {
-                    // Efficiently copy data to pre-allocated array without memory allocation
-                   dataStream.CopyLatestDataTo(channel, _linearizedDataArrays[channel], DisplayElements);
-                }
-                if(VerticalControl.IsAutoScale)
-                    WpfPlot1.Plot.Axes.AutoScaleY();
-
-                WpfPlot1.Refresh();
-                
-            }, System.Windows.Threading.DispatcherPriority.Render);
-        }
-
 
         private void UpdateAquisition(object sender, ElapsedEventArgs e)
         {
@@ -322,10 +162,10 @@ namespace SerialPlotDN_WPF
                 channelLabels.Add(new XElement("Label", channel.Label));
             }
 
-            int plotUpdateRateFPS = (int)(1000.0 / _updatePlotTimer.Interval);
+            int plotUpdateRateFPS = _plotManager.CurrentPlotUpdateRateFPS;
             int serialPortUpdateRateHz = dataStream?.SerialPortUpdateRateHz ?? 1000;
-            int lineWidth = (int)(_signals[0]?.LineWidth ?? 1);
-            bool antiAliasing = _signals[0]?.LineStyle.AntiAlias ?? false;
+            int lineWidth = _plotManager.CurrentLineWidth;
+            bool antiAliasing = _plotManager.CurrentAntiAliasing;
             bool showRenderTime = WpfPlot1.Plot.Benchmark.IsVisible;
             bool autoScale = VerticalControl.IsAutoScale;
             var settingsXml = new XElement("PlotSettings",
@@ -352,8 +192,6 @@ namespace SerialPlotDN_WPF
             if (!File.Exists(filePath)) return;
             try
             {
-
-
                 var settingsXml = XElement.Load(filePath);
                 int plotUpdateRateFPS = int.Parse(settingsXml.Element("PlotUpdateRateFPS")?.Value ?? "30");
                 int serialPortUpdateRateHz = int.Parse(settingsXml.Element("SerialPortUpdateRateHz")?.Value ?? "1000");
@@ -368,20 +206,10 @@ namespace SerialPlotDN_WPF
                 VerticalControl.Min = Ymin;
                 VerticalControl.Max = Ymax;
                 VerticalControl.IsAutoScale = autoScale;
-                _updatePlotTimer.Interval = 1000.0 / plotUpdateRateFPS;
+                _plotManager.ApplyPlotSettings(plotUpdateRateFPS, lineWidth, antiAliasing, showRenderTime);
                 if (dataStream != null)
                     dataStream.SerialPortUpdateRateHz = serialPortUpdateRateHz;
-                for (int i = 0; i < _signals.Length; i++)
-                {
-                    if (_signals[i] != null)
-                    {
-                        _signals[i].LineWidth = lineWidth;
-                        _signals[i].LineStyle.AntiAlias = antiAliasing;
-                    }
-                }
-                WpfPlot1.Plot.Benchmark.IsVisible = showRenderTime;
                 WpfPlot1.Plot.Axes.SetLimitsY(Ymin, Ymax);
-
                 var channelLabelElement = settingsXml.Element("ChannelLabels");
                 if (channelLabelElement != null)
                 {
@@ -403,47 +231,35 @@ namespace SerialPlotDN_WPF
         protected override void OnClosed(EventArgs e)
         {
             writeSettingsToXML(); // Save settings on exit
-            _updatePlotTimer?.Stop();
             _updateAquisitionTimer?.Stop();
             dataStream?.Dispose();
             base.OnClosed(e);
         }
 
-        // Event handlers for XAML events
-        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-            // Window loaded logic can go here if needed in the future
-        }
-
         private void closing(object sender, CancelEventArgs e)
         {
             _updateAquisitionTimer?.Stop();
-            _updatePlotTimer?.Stop();
             dataStream?.Dispose();
         }
 
         private void WpfPlot1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             // Restore default axis limits as in initialization
-            WpfPlot1.Plot.Axes.SetLimitsX(0, DisplayElements);
-            WpfPlot1.Plot.Axes.SetLimitsY(Ymin, Ymax);
-            WpfPlot1.Refresh();
+            _plotManager.SetYLimits(_plotManager.Ymin, _plotManager.Ymax);
         }
 
         private void WpfPlot1_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
             // Restore default axis limits as in initialization
-            WpfPlot1.Plot.Axes.SetLimitsX(0, DisplayElements);
-            WpfPlot1.Plot.Axes.SetLimitsY(Ymin, Ymax);
-            WpfPlot1.Refresh();
+            _plotManager.SetYLimits(_plotManager.Ymin, _plotManager.Ymax);
         }
 
         private void Button_ConfigPlot_Click(object sender, RoutedEventArgs e)
         {
             var settingsWindow = new View.UserForms.PlotSettingsWindow();
-            int currentFPS = (int)(1000.0 / _updatePlotTimer.Interval);
-            int currentLineWidth = (int)(_signals[0]?.LineWidth ?? 1);
-            bool currentAntiAliasing = _signals[0]?.LineStyle.AntiAlias ?? false; // Fixed property name
+            int currentFPS = _plotManager.CurrentPlotUpdateRateFPS;
+            int currentLineWidth = _plotManager.CurrentLineWidth;
+            bool currentAntiAliasing = _plotManager.CurrentAntiAliasing;
             settingsWindow.InitializeFromMainWindow(currentFPS, dataStream.SerialPortUpdateRateHz, currentLineWidth, currentAntiAliasing, WpfPlot1.Plot.Benchmark.IsVisible);
             settingsWindow.OnSettingsApplied += (settings) => ApplyPlotSettings(settings);
             settingsWindow.Show();
@@ -465,19 +281,7 @@ namespace SerialPlotDN_WPF
 
         private void ApplyPlotSettings(View.UserForms.PlotSettingsWindow settings)
         {
-            _updatePlotTimer.Interval = 1000.0 / settings.PlotUpdateRateFPS;
-            dataStream.SerialPortUpdateRateHz = settings.SerialPortUpdateRateHz;
-            WpfPlot1.Plot.Grid.LineWidth = (float)settings.LineWidth / 2;
-            for (int i = 0; i < _signals.Length; i++)
-            {
-                if (_signals[i] != null)
-                {
-                    _signals[i].LineWidth = (float)settings.LineWidth;
-                    _signals[i].LineStyle.AntiAlias = settings.AntiAliasing;
-                }
-            }
-            WpfPlot1.Plot.Benchmark.IsVisible = settings.ShowRenderTime;
-            WpfPlot1.Refresh();
+            _plotManager.ApplyPlotSettings(settings.PlotUpdateRateFPS, settings.LineWidth, settings.AntiAliasing, settings.ShowRenderTime);
         }
 
         private void Scrollbar_ValueChanged(object sender, System.Windows.Controls.Primitives.ScrollEventArgs e)
--- Model/DataAndPlotManager.cs
+++ Model/DataAndPlotManager.cs
@@ -1,68 +1,164 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ScottPlot.Plottables;
 using System.Windows;
 using System.Timers;
-using ScottPlot.WPF;
 using ScottPlot;
+using ScottPlot.Plottables;
+using ScottPlot.WPF;
+using System.Windows.Media;
+using SerialPlotDN_WPF.View.UserControls;
+using Color = System.Windows.Media.Color;
 
 namespace SerialPlotDN_WPF.Model
 {
     public class DataAndPlotManager
     {
-        private readonly System.Timers.Timer AddNewDataTimer = new() { Interval = 10, Enabled = true, AutoReset = true };
-        private readonly System.Timers.Timer UpdatePlotTimer = new() { Interval = 20, Enabled = true, AutoReset = true };
-        private readonly ScottPlot.Plottables.DataStreamer Streamer;
-        public const int dataToBeAdded = 100;
+        private readonly System.Timers.Timer _updatePlotTimer;
+        private readonly WpfPlotGL WpfPlot1;
+        private readonly ChannelControlBar ChannelControlBar;
+        private readonly VerticalControl VerticalControl;
+        private readonly HorizontalControl HorizontalControl;
+        private readonly Signal[] _signals;
+        private readonly double[][] _linearizedDataArrays;
+        private readonly int _channelCount;
+        public int DisplayElements { get; set; } = 3000;
+        public int Ymin { get; set; } = -200;
+        public int Ymax { get; set; } = 4000;
 
-        //private readonly ref struct 
-        private WpfPlotGL plot;
-        
-        public DataAndPlotManager(ref ScottPlot.WPF.WpfPlotGL plot) 
+        public DataAndPlotManager(WpfPlotGL wpfPlot1, ChannelControlBar channelControlBar, VerticalControl verticalControl, HorizontalControl horizontalControl, int channelCount = 8)
         {
-            //this.plot = plot;
-
-            plot.Refresh();
-            this.plot = plot;
-
-            Streamer = plot.Plot.Add.DataStreamer(5000);
-            Streamer.ViewScrollLeft();
-
-            InitTimer();
+            WpfPlot1 = wpfPlot1;
+            ChannelControlBar = channelControlBar;
+            VerticalControl = verticalControl;
+            HorizontalControl = horizontalControl;
+            _channelCount = channelCount;
+            _signals = new Signal[_channelCount];
+            _linearizedDataArrays = new double[_channelCount][];
+            _updatePlotTimer = new System.Timers.Timer(33) { Enabled = true, AutoReset = true };
+            _updatePlotTimer.Elapsed += UpdatePlot;
         }
 
-        private void InitTimer()
+        public void InitializePlot()
         {
-            AddNewDataTimer.Elapsed += AddNewData;
-            AddNewDataTimer.Start();
-
-            UpdatePlotTimer.Elapsed += UpdatePlot;
-            UpdatePlotTimer.Start();
-        }
-
-        private void AddNewData(Object source, ElapsedEventArgs e)
-        {
-            double[] valuesA = new double[dataToBeAdded];
-            for (int i = 0; i < dataToBeAdded; i++)
+            WpfPlot1.Plot.Clear();
+            WpfPlot1.Plot.Add.Palette = new ScottPlot.Palettes.Category10();
+            foreach (var axis in WpfPlot1.Plot.Axes.GetAxes())
             {
-
-                valuesA[i] = 10 * Math.Sin(2 * Math.PI * (double)i / dataToBeAdded) + 0.01 * DataGenerator.GenerateRandomNumber();
+                axis.TickLabelStyle.FontSize *= 2;
+                axis.Label.FontSize *= 2;
             }
-            Streamer.AddRange(valuesA);
-        }
-
-        private void UpdatePlot(Object source, ElapsedEventArgs e)
-        {
-            if (Streamer.HasNewData)
+            for (int i = 0; i < _channelCount; i++)
             {
-                this.plot.Refresh();
+                _linearizedDataArrays[i] = new double[DisplayElements];
+                _signals[i] = WpfPlot1.Plot.Add.Signal(_linearizedDataArrays[i]);
+                _signals[i].Color = _signals[i].Color.Lighten(0.2);
+                _signals[i].Color = _signals[i].Color.WithOpacity(1.0);
+                _signals[i].LineWidth = 1;
+                _signals[i].LineStyle.AntiAlias = false;
+            }
 
+            WpfPlot1.Plot.FigureBackground.Color = ScottPlot.Color.FromHex("#181818");
+            WpfPlot1.Plot.DataBackground.Color = ScottPlot.Color.FromHex("#1f1f1f");
+            WpfPlot1.Plot.Axes.Color(ScottPlot.Color.FromHex("#d7d7d7"));
+            WpfPlot1.Plot.Grid.LineWidth = 1.0f;
+            WpfPlot1.Plot.Grid.MajorLineColor = ScottPlot.Color.FromHex("#404040");
+            WpfPlot1.Plot.Legend.BackgroundColor = ScottPlot.Color.FromHex("#404040");
+            WpfPlot1.Plot.Legend.FontColor = ScottPlot.Color.FromHex("#d7d7d7");
+            WpfPlot1.Plot.Legend.OutlineColor = ScottPlot.Color.FromHex("#d7d7d7");
+            WpfPlot1.Plot.Axes.ContinuouslyAutoscale = false;
+            WpfPlot1.Plot.RenderManager.ClearCanvasBeforeEachRender = true;
+            WpfPlot1.Plot.Axes.SetLimitsX(0, DisplayElements);
+            WpfPlot1.Plot.Axes.SetLimitsY(Ymin, Ymax);
+            WpfPlot1.Plot.Axes.Bottom.IsVisible = true;
+        }
+
+        public void InitializeChannelControlBar()
+        {
+            for (int i = 0; i < _channelCount; i++)
+            {
+                ChannelControl channel = new ChannelControl();
+                var channelColor = _signals[i].Color;
+                var wpfColor = Color.FromArgb(channelColor.A, channelColor.R, channelColor.G, channelColor.B);
+                channel.Color = wpfColor;
+                channel.Label = $"CH{i + 1}";
+                channel.Gain = 1.0;
+                channel.Offset = 0.0;
+                ChannelControlBar.AddChannel(channel);
             }
         }
 
+        public void StartPlotTimer() => _updatePlotTimer.Start();
+        public void StopPlotTimer() => _updatePlotTimer.Stop();
 
+        public void UpdatePlot(object? source, ElapsedEventArgs? e)
+        {
+            Application.Current.Dispatcher.BeginInvoke(() =>
+            {
+                if (VerticalControl.IsAutoScale)
+                    WpfPlot1.Plot.Axes.AutoScaleY();
+                WpfPlot1.Refresh();
+            }, System.Windows.Threading.DispatcherPriority.Render);
+        }
+
+        public void OnWindowSizeChanged(object? sender, int newSize)
+        {
+            DisplayElements = newSize;
+            bool isRunning = _updatePlotTimer.Enabled;
+            if (_updatePlotTimer.Enabled)
+                _updatePlotTimer.Stop();
+            for (int i = 0; i < _channelCount; i++)
+            {
+                var colorOld = _signals[i].Color;
+                var linewidthOld = _signals[i].LineWidth;
+                var antiAliasingOld = _signals[i].LineStyle.AntiAlias;
+                WpfPlot1.Plot.Remove(_signals[i]);
+                _linearizedDataArrays[i] = new double[DisplayElements];
+                _signals[i] = WpfPlot1.Plot.Add.Signal(_linearizedDataArrays[i]);
+                _signals[i].Color = colorOld;
+                _signals[i].LineWidth = linewidthOld;
+                _signals[i].LineStyle.AntiAlias = antiAliasingOld;
+            }
+            WpfPlot1.Plot.Axes.SetLimitsX(0, DisplayElements);
+            if (isRunning)
+                _updatePlotTimer.Start();
+            else
+                UpdatePlot(null, null);
+        }
+
+        public void ApplyPlotSettings(int plotUpdateRateFPS, int lineWidth, bool antiAliasing, bool showRenderTime)
+        {
+            _updatePlotTimer.Interval = 1000.0 / plotUpdateRateFPS;
+            WpfPlot1.Plot.Grid.LineWidth = (float)lineWidth / 2;
+            for (int i = 0; i < _signals.Length; i++)
+            {
+                if (_signals[i] != null)
+                {
+                    _signals[i].LineWidth = (float)lineWidth;
+                    _signals[i].LineStyle.AntiAlias = antiAliasing;
+                }
+            }
+            WpfPlot1.Plot.Benchmark.IsVisible = showRenderTime;
+            WpfPlot1.Refresh();
+        }
+
+        public void SetupPlotUserInput()
+        {
+            WpfPlot1.UserInputProcessor.Reset();
+            WpfPlot1.UserInputProcessor.IsEnabled = false;
+            var zoomRectangleButton = ScottPlot.Interactivity.StandardMouseButtons.Right;
+            var zoomRectangleResponse = new ScottPlot.Interactivity.UserActionResponses.MouseDragZoomRectangle(zoomRectangleButton);
+            WpfPlot1.UserInputProcessor.UserActionResponses.Add(zoomRectangleResponse);
+        }
+
+        public void SetYLimits(int ymin, int ymax)
+        {
+            Ymin = ymin;
+            Ymax = ymax;
+            WpfPlot1.Plot.Axes.SetLimitsY(Ymin, Ymax);
+            WpfPlot1.Refresh();
+        }
+
+        public int CurrentPlotUpdateRateFPS => (int)(1000.0 / _updatePlotTimer.Interval);
+        public int CurrentLineWidth => (int)(_signals[0]?.LineWidth ?? 1);
+        public bool CurrentAntiAliasing => _signals[0]?.LineStyle.AntiAlias ?? false;
     }
 }
